f07f37aa4e76e48980595d756c5aa74c8a0ba773
nerthul11 <leandrorositto92@gmail.com>
2025-09-01 08:29:09 -0300

    b

--- BreakableWallRandomizer.cs
+++ BreakableWallRandomizer.cs
@@ -9,7 +9,7 @@ namespace BreakableWallRandomizer
     public class BreakableWallRandomizer : Mod, IGlobalSettings<BWR_Settings>
     {
         new public string GetName() => "Breakable Wall Randomizer";
-        public override string GetVersion() => "4.1.1.3";
+        public override string GetVersion() => "4.1.1.4";
         public BWR_Settings GS { get; set; } = new();
         private static BreakableWallRandomizer _instance;
         public BreakableWallRandomizer() : base()
